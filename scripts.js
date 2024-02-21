2a5d2bc9661c605d8a960fa89e11cf5f6c4040b6
hustnhdev <159974932+hustnhdev@users.noreply.github.com>
2024-02-22 06:51:57 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function openPopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'flex';
-}
-
-function closePopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'none';
-}
